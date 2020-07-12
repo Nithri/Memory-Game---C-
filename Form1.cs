b18887153d898e24b7dc8da018bb2f45382b6ac3
Nithri <karolina.wojtyla.terka@gmail.com>
2020-07-12 10:38:42 +0200

    wybranie obrazkow i utworzenie listy

--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,12 @@ namespace Memory_Game_1
 {
     public partial class Form1 : Form
     {
+        Random random = new Random();
+
+        List<string> icons = new List<string>()
+        {
+            "J","J","P","P","M","M","E","E","j","j","o","o","b","b","v","v"
+        };
         public Form1()
         {
             InitializeComponent();
